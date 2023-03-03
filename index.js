01f6457bb77eb409968fb345b4500eb5f890c14d
Can <63351166+fastuptime@users.noreply.github.com>
2023-03-03 18:18:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Bot Aktif!");
+});
+
+client.on("message", (message) => {
+  const msg = message.body.toLocaleLowerCase();
+  if (msg.startsWith(".test")) {
+    message.reply("Test");
+  }
+});
+
+client.initialize();
